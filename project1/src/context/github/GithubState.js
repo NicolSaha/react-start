f2eea4f3837d9bb6df8d57d68ee3c6fd8a2bd454
“[Nicol <[nicol.saha@gmail.com]>
2020-12-22 20:15:45 +0100

    Move user state to context

--- project1/src/context/github/GithubState.js
+++ project1/src/context/github/GithubState.js
@@ -35,8 +35,23 @@ const GithubState = (props) => {
   };
 
   // GET USER
+  const getUser = async (username) => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_USER,
+      payload: res.data,
+    });
+  };
+
   // GET REPOS
   // CLEAR USERS
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
+
   // SET LOADING
   const setLoading = () => dispatch({ type: SET_LOADING });
 
@@ -48,6 +63,8 @@ const GithubState = (props) => {
         repos: state.repos,
         loading: state.loading,
         searchUsers,
+        clearUsers,
+        getUser,
       }}
     >
       {props.children}
